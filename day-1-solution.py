7ea6bfd3ecf4c228fead43145ac00942e495d5ba
nityansuman <nityan.suman@gmail.com>
2021-06-27 23:33:50 +0530

    Updated comments

--- day-1-solution.py
+++ day-1-solution.py
@@ -1,19 +1,19 @@
-# Declare first integer, double and string variables.
+# Declare first integer, double and string variables
 i = 4
 d = 4.0
 s = "HackerRank "
 
-# Read and save an integer, double, and String to your variables.
+# Read and save an integer, double, and String to your variables
 input_i = int(input())
 input_d = float(input())
 input_s = input()
 
-# Print the sum of both integer variables on a new line.
+# Print the sum of both integer variables on a new line
 print(i + input_i)
 
-# Print the sum of the double variables on a new line.
+# Print the sum of the double variables on a new line
 print(d + input_d)
 
 # Concatenate and print the String variables on a new line
-# The 's' variable above should be printed first.
+# The 's' variable above should be printed first
 print(s + input_s)
