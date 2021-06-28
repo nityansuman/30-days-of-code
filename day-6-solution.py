a7b6c668b99ef9167deab29b04641d169b4c3a1c
nityansuman <nityan.suman@gmail.com>
2021-06-29 00:57:52 +0530

    Adding day 6 exercise

---
+++ day-6-solution.py
@@ -0,0 +1,22 @@
+
+if __name__ == "__main__":
+	# Read test case input from stdin
+	T = int(input())
+
+	# For each test case
+	for i in range(T):
+
+		# Read input from stdin
+		s = input()
+
+		# Placeholders
+		even_string, odd_string = "", ""
+
+		# Compute strings
+		for j in range(len(s)):
+			if j%2 == 0:
+				even_string += s[j]
+			else:
+				odd_string += s[j]
+
+		print(f"{even_string} {odd_string}")
